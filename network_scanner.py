05f82f3d7f2dadbbab522973da796137c7831823
Cfloy66 <christopherfloyd16@gmail.com>
2020-12-27 15:58:41 -0500

    created a dictionary and list for the client addresses

--- network_scanner.py
+++ network_scanner.py
@@ -7,12 +7,20 @@ def scan(ip):
 	arp_request = scapy.ARP(pdst=ip)
 	broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
 	arp_request_broadcast = broadcast/arp_request
-	answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+	answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
+	clients_list = []
 	for element in answered_list:
-		print(element[1].psrc, "IP address")
-		print(element[1].hwsrc, "MAC address")
-		print("===========================================================")
+		client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+		clients_list.append(client_dict)
+	return clients_list
 
 
-scan("10.0.2.1/24")
+def print_result(results_list):
+	print("IP\t\t\tMAC Address\n==============================")
+	for client in results_list:
+		print(client["ip"] + "\t\t" + client["mac"])
+
+
+scan_result = scan("10.0.2.1/24")
+print_result(scan_result)
